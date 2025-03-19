96f7549e02d8b50ccec2f5dc5e1f05303641c421
eediallo <elhhabdouldiallo@gmail.com>
2025-03-19 07:09:00 +0000

    add message router to app and remove inline message routes

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./db/db.js";
+import { messageRouter } from "./routers/message.js";
 dotenv.config();
 
 const app = express();
@@ -10,6 +11,8 @@ const port = process.env.PORT || 3000;
 //middleware
 app.use(express.json());
 
+app.use("/api/v1/messages", messageRouter);
+
 //routes
 app.get("/", (req, res) => {
   res
@@ -17,16 +20,6 @@ app.get("/", (req, res) => {
     .send(
       '<h1>Welcome to our live chat application</h1><a href="/api/v1/messages">Go to messages</a>'
     );
-});
-
-// get all messages
-app.get("/api/v1/messages", (req, res) => {
-  res.status(200).json({ msg: "Messages here" });
-});
-
-//send a message
-app.post("/api/v1/messages/send-message", (req, res) => {
-  res.status(201).json(req.body);
 });
 
 const start = async () => {
