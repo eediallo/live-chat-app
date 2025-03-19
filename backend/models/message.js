17c31aeb672236cdea6549b6f8cca648c09dfb23
eediallo <elhhabdouldiallo@gmail.com>
2025-03-19 07:28:15 +0000

    capitalize model name in message schema export

--- backend/models/message.js
+++ backend/models/message.js
@@ -17,4 +17,4 @@ const messageSchema = new mongoose.Schema({
   },
 });
 
-export const Message = mongoose.model("message", messageSchema);
+export const Message = mongoose.model("Message", messageSchema);
