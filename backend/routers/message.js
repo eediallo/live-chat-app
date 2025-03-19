5754de029efea1ca564d06dbe4f9ab4a727ffbfb
eediallo <elhhabdouldiallo@gmail.com>
2025-03-19 07:29:42 +0000

    remove message router file as it is no longer needed

--- backend/routers/message.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { getAllMessages, sendMessage } from "../controllers/message.js";
-
-const messageRouter = express.Router();
-
-messageRouter.get("/", getAllMessages);
-messageRouter.post("/send-message", sendMessage);
-
-export { messageRouter };
