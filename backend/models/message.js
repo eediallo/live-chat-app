9f6a2405b5245432ffda43c398d61ae24d0c50b3
eediallo <elhhabdouldiallo@gmail.com>
2025-03-19 07:09:26 +0000

    add message model with schema for text, user, and createdAt fields

---
+++ backend/models/message.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+
+const messageSchema = new mongoose.Schema({
+  text: {
+    type: String,
+    trim: true,
+    require: [true, "Text message must be provided"],
+  },
+  user: {
+    type: String,
+    trim: true,
+    require: [true, "User must be provided"],
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+});
+
+export const Message = mongoose.model("message", messageSchema);
