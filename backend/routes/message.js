123a2808d5785f27143d9d163143102b3f2c274b
eediallo <elhhabdouldiallo@gmail.com>
2025-03-19 09:34:11 +0000

    add updateMessage route to handle message updates in the backend

--- backend/routes/message.js
+++ backend/routes/message.js
@@ -3,6 +3,7 @@ import {
   getAllMessages,
   deleteMessage,
   sendMessage,
+  updateMessage,
 } from "../controllers/message.js";
 
 const messageRouter = express.Router();
@@ -10,4 +11,5 @@ const messageRouter = express.Router();
 messageRouter.get("/", getAllMessages);
 messageRouter.post("/send-message", sendMessage);
 messageRouter.patch("/:id", deleteMessage);
+messageRouter.put('/:id', updateMessage)
 export { messageRouter };
