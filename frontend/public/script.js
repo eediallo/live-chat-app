3903077e45a327eb471872edf26902f6d8782370
eediallo <elhhabdouldiallo@gmail.com>
2025-03-19 08:42:46 +0000

    add message fetching and rendering functionality in script.js

---
+++ frontend/public/script.js
@@ -0,0 +1,38 @@
+const baseUrl = "http://localhost:3000";
+
+async function fetchMessages() {
+  try {
+    const res = await fetch(`${baseUrl}/api/v1/messages`);
+    if (!res.ok) {
+      throw new Error(`Failed to fetch message: ${res.status}`);
+    }
+    const { messages } = await res.json();
+    return messages;
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+function createMessageCard(message) {
+  const messageSection = document.createElement("section");
+  const textMessage = document.createElement("p");
+  textMessage.textContent = message.text;
+  const user = document.createElement("p");
+  user.textContent = message.user;
+
+  messageSection.append(textMessage, user);
+  return messageSection;
+}
+
+function render(messages) {
+  const listOfMessages = messages.map(createMessageCard);
+  document.querySelector("body").append(...listOfMessages);
+}
+
+async function main() {
+  const messages = await fetchMessages();
+  console.log(messages);
+  render(messages);
+}
+
+window.onload = main;
