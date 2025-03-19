2a70ef93899d4867387694bb3da2d6346af01f84
eediallo <elhhabdouldiallo@gmail.com>
2025-03-19 14:00:23 +0000

    add send message functionality with user input handling

--- frontend/public/script.js
+++ frontend/public/script.js
@@ -1,8 +1,12 @@
-const baseUrl = "http://localhost:3000/api/v1/messages";
+const userInput = document.querySelector("#user-name");
+const textMessageInput = document.querySelector("#text-message");
+const sendMsgBtn = document.querySelector("#send-msg-btn");
+
+const baseUrl = "http://localhost:3000";
 
 async function fetchMessages() {
   try {
-    const res = await fetch(`${baseUrl}`);
+    const res = await fetch(`${baseUrl}/api/v1/messages`);
     if (!res.ok) {
       throw new Error(`Failed to fetch message: ${res.status}`);
     }
@@ -10,6 +14,25 @@ async function fetchMessages() {
     return messages;
   } catch (err) {
     console.error(err);
+  }
+}
+
+async function sendMessage(textMessage, userName) {
+  try {
+    const resp = await fetch(baseUrl, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ text: textMessage, user: userName }),
+    });
+    if (!resp.ok) {
+      throw new Error(`Failed to send message: ${resp.status}`);
+    }
+    const data = await resp.json();
+    console.log(data);
+  } catch (err) {
+    console.error(err.msg);
   }
 }
 
@@ -25,6 +48,17 @@ function render(messages) {
   const listOfMessages = messages.map(createMessageCard);
   document.querySelector("body").append(...listOfMessages);
 }
+
+//send message event
+sendMsgBtn.addEventListener("click", async (e) => {
+  e.preventDefault();
+  const textMessage = textMessageInput.value.trim();
+  console.log(textMessage)
+  const user = userInput.value.trim();
+  console.log(user)
+  console.log("sending message.....");
+  await sendMessage(textMessage, user);
+});
 
 async function main() {
   const messages = await fetchMessages();
