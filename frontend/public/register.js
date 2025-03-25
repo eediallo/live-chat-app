9dcc1783b7f17e084bddc60abe8ac86a5b5b2a35
eediallo <elhhabdouldiallo@gmail.com>
2025-03-25 12:29:27 +0000

    add error handling to registration function and display error messages

--- frontend/public/register.js
+++ frontend/public/register.js
@@ -4,6 +4,7 @@ const emailInput = document.getElementById("email");
 const passwordInput = document.getElementById("password");
 const nameInput = document.getElementById("name");
 const registerBtn = document.getElementById("register-btn");
+const errMsg = document.getElementById("msg");
 
 const register = async (e) => {
   e.preventDefault();
@@ -11,22 +12,27 @@ const register = async (e) => {
   const email = emailInput.value;
   const password = passwordInput.value;
   const body = { name, email, password };
-  const response = await fetch(endpoint, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(body),
-  });
-  const data = await response.json();
-  console.log(data);
 
-  if (response.ok) {
-    alert("User registered successfully");
-    localStorage.setItem("token", data.token);
-    window.location.href = "/login.html";
-  } else {
-    document.getElementById("msg").innerText = data.msg;
+  try {
+    const response = await fetch(endpoint, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(body),
+    });
+    const data = await response.json();
+
+    if (response.ok) {
+      alert("User registered successfully");
+      localStorage.setItem("token", data.token);
+      window.location.href = "/login.html";
+    } else {
+      document.getElementById("msg").innerText = data.msg;
+    }
+  } catch (error) {
+    console.error("Error occurred:", error);
+    errMsg.innerText = data.msg;
   }
 };
 
