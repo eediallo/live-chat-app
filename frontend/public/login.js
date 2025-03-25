0b3f496c69182f97717c5acc64c85b44b2953bdc
eediallo <elhhabdouldiallo@gmail.com>
2025-03-25 12:27:24 +0000

    add error handling to login function and display error messages

--- frontend/public/login.js
+++ frontend/public/login.js
@@ -3,27 +3,32 @@ const endpoint = "http://localhost:3000/api/v1/auth/login";
 const loginBtn = document.getElementById("login-btn");
 const emailInput = document.getElementById("email");
 const passwordInput = document.getElementById("password");
+const msgEl = document.getElementById("msg");
 
 const login = async (e) => {
   e.preventDefault();
-  const email = emailInput.value;
-  const password = passwordInput.value;
-  const body = { email, password };
-  const response = await fetch(endpoint, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(body),
-  });
-  const data = await response.json();
-  console.log(data);
-  if (response.ok) {
-    alert("User logged in successfully");
-    localStorage.setItem("token", data.token);
-    window.location.href = "/api/v1/messages";
-  } else {
-    document.getElementById("msg").innerText = data.msg;
+  try {
+    const email = emailInput.value;
+    const password = passwordInput.value;
+    const body = { email, password };
+    const response = await fetch(endpoint, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(body),
+    });
+    const data = await response.json();
+    if (response.ok) {
+      alert("User logged in successfully");
+      localStorage.setItem("token", data.token);
+      window.location.href = "/api/v1/messages";
+    } else {
+      document.getElementById("msg").innerText = data.msg;
+    }
+  } catch (error) {
+    console.error("An error occurred:", error);
+    msgEl.innerText = data.msg;
   }
 };
 
