bd1158c41bdf42220a75b3925dc0891c0479fbd2
eediallo <elhhabdouldiallo@gmail.com>
2025-03-25 12:13:36 +0000

    add registration functionality with API integration and user feedback

---
+++ frontend/public/register.js
@@ -0,0 +1,33 @@
+const endpoint = "http://localhost:3000/api/v1/auth/register";
+
+const emailInput = document.getElementById("email");
+const passwordInput = document.getElementById("password");
+const nameInput = document.getElementById("name");
+const registerBtn = document.getElementById("register-btn");
+
+const register = async (e) => {
+  e.preventDefault();
+  const name = nameInput.value;
+  const email = emailInput.value;
+  const password = passwordInput.value;
+  const body = { name, email, password };
+  const response = await fetch(endpoint, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(body),
+  });
+  const data = await response.json();
+  console.log(data);
+
+  if (response.ok) {
+    alert("User registered successfully");
+    localStorage.setItem("token", data.token);
+    window.location.href = "/api/v1/messages";
+  } else {
+    document.getElementById("msg").innerText = data.msg;
+  }
+};
+
+registerBtn.addEventListener("click", register);
