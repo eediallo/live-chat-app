932dc6f2cf966dc4d6fc37f0eb4d52af929465f4
eediallo <elhhabdouldiallo@gmail.com>
2025-03-25 21:58:44 +0000

    fix: replace localStorage token handling with setToken function from storage.js

--- frontend/public/scripts/login.js
+++ frontend/public/scripts/login.js
@@ -1,4 +1,5 @@
 const endpoint = "http://localhost:3000/api/v1/auth/login";
+import { setToken } from "./storage.js";
 
 const loginBtn = document.getElementById("login-btn");
 const emailInput = document.getElementById("email");
@@ -18,17 +19,16 @@ const login = async (e) => {
       },
       body: JSON.stringify(body),
     });
-    const data = await response.json();
+    const { token } = await response.json();
     if (response.ok) {
-      alert("User logged in successfully");
-      localStorage.setItem("token", data.token);
+      setToken(token);
       window.location.href = "/dashboard.html";
     } else {
-      document.getElementById("msg").innerText = data.msg;
+      document.getElementById("msg").innerText = "Invalid email or password";
     }
   } catch (error) {
     console.error("An error occurred:", error);
-    msgEl.innerText = data.msg;
+    msgEl.innerText = "An error occurred. Please try again later.";
   }
 };
 
