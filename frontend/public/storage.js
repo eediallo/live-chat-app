ee0d147f4b4d6afe69d4cf4ab83dcbf11eb813fb
eediallo <elhhabdouldiallo@gmail.com>
2025-03-25 15:44:46 +0000

    feat: add token management functions for local storage in storage.js

---
+++ frontend/public/storage.js
@@ -0,0 +1,13 @@
+function getToken() {
+  return localStorage.getItem("token");
+}
+
+function setToken(token) {
+  localStorage.setItem("token", token);
+}
+
+function removeToken() {
+  localStorage.removeItem("token");
+}
+
+export { getToken, setToken, removeToken };
