8bf915cb3c9e0547685676df4ea292b4fa3b4f84
eediallo <elhhabdouldiallo@gmail.com>
2025-04-01 18:31:14 +0100

    refactor: remove authentication router and related middleware

--- webSocket-without-auth/backend/app.js
+++ webSocket-without-auth/backend/app.js
@@ -3,12 +3,9 @@ import dotenv from "dotenv";
 import cors from "cors";
 import { connectDB } from "./db/db.js";
 import { messageRouter } from "./routes/messages.js";
-import { authRouter } from "./routes/auth.js";
 import { WebSocketServer } from "ws";
 import http from "http";
 dotenv.config();
-// import { notFound } from "./middleware/notFound.js";
-// import { errorHandlerMiddleware } from "./middleware/errorHandler.js";
 
 const app = express();
 const server = http.createServer(app);
@@ -42,15 +39,11 @@ const port = process.env.PORT || 3000;
 const publicDir = new URL("../frontend/public", import.meta.url).pathname;
 
 //middleware
-app.use(cors()); // use cors
-app.use(express.json()); //parse json
+app.use(cors()); 
+app.use(express.json()); 
 app.use(express.static(publicDir)); // serve static files
 
 app.use("/api/v1/messages", messageRouter);
-app.use("/api/v1/auth", authRouter);
-
-// app.use(notFound);
-// app.use(errorHandlerMiddleware);
 
 const start = async () => {
   try {
