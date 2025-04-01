f5e6983bf56d222ab2fa47d0ac816b3b31c6c6d1
eediallo <elhhabdouldiallo@gmail.com>
2025-04-01 18:29:55 +0100

    refactor: simplify message routes by removing authentication middleware

--- webSocket-without-auth/backend/routes/messages.js
+++ webSocket-without-auth/backend/routes/messages.js
@@ -1,20 +1,13 @@
 import express from "express";
 import {
-  deleteMessage,
-  updateMessage,
-  getMessage,
   createMessage,
   getAllMessages,
   getAllMessagesForAllUsers,
 } from "../controllers/messages.js";
-import { authenticateUser } from "../middleware/auth.js";
 
 const messageRouter = express.Router();
 
 messageRouter.get("/all", getAllMessagesForAllUsers);
-messageRouter.get("/", authenticateUser, getAllMessages);
-messageRouter.post("/", authenticateUser, createMessage);
-messageRouter.get("/:id", authenticateUser, getMessage);
-messageRouter.patch("/:id", authenticateUser, updateMessage);
-messageRouter.delete("/:id", authenticateUser, deleteMessage);
+messageRouter.get("/", getAllMessages);
+messageRouter.post("/", createMessage);
 export { messageRouter };
