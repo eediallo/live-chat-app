890fc8a87f13cff4f290bf3c637b9d27c70d78e8
eediallo <elhhabdouldiallo@gmail.com>
2025-04-01 17:26:08 +0100

    feat: implement WebSocket server for real-time messaging functionality

--- webSocket-without-auth/backend/app.js
+++ webSocket-without-auth/backend/app.js
@@ -4,9 +4,38 @@ import cors from "cors";
 import { connectDB } from "./db/db.js";
 import { messageRouter } from "./routes/messages.js";
 import { authRouter } from "./routes/auth.js";
+import { WebSocketServer } from "ws";
+import http from "http";
 dotenv.config();
+// import { notFound } from "./middleware/notFound.js";
+// import { errorHandlerMiddleware } from "./middleware/errorHandler.js";
 
 const app = express();
+const server = http.createServer(app);
+
+const wss = new WebSocketServer({ server });
+
+// wss connection event
+wss.on("connection", (ws) => {
+  console.log("New client connected");
+
+  // handle messages from clients
+  ws.on("message", (message) => {
+    const msgString = message.toString();
+
+    // Broadcast the message to all connected client
+    wss.clients.forEach((client) => {
+      if (client.readyState === WebSocket.OPEN) {
+        client.send(msgString);
+      }
+    });
+  });
+
+  // Handle client disconnection
+  ws.on("close", () => {
+    console.log("Client disconnected");
+  });
+});
 
 const port = process.env.PORT || 3000;
 
@@ -20,10 +49,13 @@ app.use(express.static(publicDir)); // serve static files
 app.use("/api/v1/messages", messageRouter);
 app.use("/api/v1/auth", authRouter);
 
+// app.use(notFound);
+// app.use(errorHandlerMiddleware);
+
 const start = async () => {
   try {
     await connectDB(process.env.MONGO_URI);
-    app.listen(port, () =>
+    server.listen(port, () =>
       console.log(`Server is listening on port ${port}...`)
     );
   } catch (err) {
