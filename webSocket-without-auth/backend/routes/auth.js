0953715f882558bd5337fbe90d236489c5435631
eediallo <elhhabdouldiallo@gmail.com>
2025-04-01 18:30:05 +0100

    refactor: remove authentication routes and associated logic

--- webSocket-without-auth/backend/routes/auth.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { login, register } from "../controllers/auth.js";
-
-const authRouter = express.Router();
-
-authRouter.route("/login").post(login);
-authRouter.route("/register").post(register);
-
-export { authRouter };
