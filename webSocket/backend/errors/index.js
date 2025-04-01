3b1afa022a3f3c057c2c54c67735fa6ecb0098eb
eediallo <elhhabdouldiallo@gmail.com>
2025-04-01 15:38:45 +0100

    feat: create centralized error exports for improved error handling

---
+++ webSocket/backend/errors/index.js
@@ -0,0 +1,5 @@
+import { CustomAPIError } from "./custom-error.js";
+import { BadRequest } from "./badRequestError.js";
+import { NotFound } from "./notFoundError.js";
+
+export { BadRequest, NotFound, CustomAPIError };
