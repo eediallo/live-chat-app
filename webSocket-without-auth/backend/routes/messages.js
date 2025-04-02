071aa0587299f63efb0489c69fa92eed6584ec86
eediallo <elhhabdouldiallo@gmail.com>
2025-04-02 17:10:02 +0100

    refactor: remove unused getAllMessages route from messages router

--- webSocket-without-auth/backend/routes/messages.js
+++ webSocket-without-auth/backend/routes/messages.js
@@ -1,11 +1,7 @@
 import express from "express";
-import {
-  getAllMessages,
-  getAllMessagesForAllUsers,
-} from "../controllers/messages.js";
+import { getAllMessagesForAllUsers } from "../controllers/messages.js";
 
 const messageRouter = express.Router();
 
 messageRouter.get("/all", getAllMessagesForAllUsers);
-messageRouter.get("/", getAllMessages);
 export { messageRouter };
