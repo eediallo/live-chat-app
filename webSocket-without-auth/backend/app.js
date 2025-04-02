af70321997811b473899268a8c0f3dc42f296640
eediallo <elhhabdouldiallo@gmail.com>
2025-04-02 16:40:25 +0100

    refactor: simplify message handling by extracting logic into a separate function

--- webSocket-without-auth/backend/app.js
+++ webSocket-without-auth/backend/app.js
@@ -5,8 +5,7 @@ import { connectDB } from "./db/db.js";
 import { messageRouter } from "./routes/messages.js";
 import { WebSocketServer } from "ws";
 import http from "http";
-import { User } from "./models/user.js";
-import { Message } from "./models/message.js";
+import { saveMsgFromWebsocketToDb } from "./controllers/messages.js";
 dotenv.config();
 
 const app = express();
@@ -14,33 +13,23 @@ const server = http.createServer(app);
 
 const wss = new WebSocketServer({ server });
 
-const saveMsgFromWebsocketToDb = async (message) => {
-  const { sender, text, createdAt } = message;
-  const username = sender.username;
+const handleIncomingMessage = async (message) => {
+  const msgString = message.toString();
 
-  if (!username || !text) {
-    console.error("Username and text message must be provided");
-    return;
-  }
   try {
-    let existingUser = await User.findOne({ username: username });
-    if (!existingUser) {
-      existingUser = await User.create({ username: username });
+    const messageData = JSON.parse(msgString);
+    const newMessage = await saveMsgFromWebsocketToDb(messageData);
+
+    // Broadcast the message to all connected client
+    if (newMessage) {
+      wss.clients.forEach((client) => {
+        if (client.readyState === WebSocket.OPEN) {
+          client.send(JSON.stringify(newMessage));
+        }
+      });
     }
-
-    const newMessage = await Message.create({
-      sender: {
-        id: existingUser._id,
-        username: username,
-      },
-      text,
-      createdAt,
-    });
-
-    return newMessage;
-  } catch (e) {
-    console.log(e);
-    return null;
+  } catch (error) {
+    console.error("Error parsing message", error);
   }
 };
 
@@ -49,25 +38,7 @@ wss.on("connection", (ws) => {
   console.log("New client connected");
 
   // handle messages from clients
-  ws.on("message", async (message) => {
-    const msgString = message.toString();
-
-    try {
-      const messageData = JSON.parse(msgString);
-      const newMessage = await saveMsgFromWebsocketToDb(messageData);
-
-      // Broadcast the message to all connected client
-      if (newMessage) {
-        wss.clients.forEach((client) => {
-          if (client.readyState === WebSocket.OPEN) {
-            client.send(JSON.stringify(newMessage));
-          }
-        });
-      }
-    } catch (error) {
-      console.error("Error parsing message", error);
-    }
-  });
+  ws.on("message", handleIncomingMessage);
 
   // Handle client disconnection
   ws.on("close", () => {
