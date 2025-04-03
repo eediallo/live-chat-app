fa1e6ba93e736099f8673c88a2fe175d6ddec009
eediallo <elhhabdouldiallo@gmail.com>
2025-04-03 13:21:41 +0100

    feat: enhance WebSocket message handling to support like and dislike reactions

--- webSocket-without-auth/backend/handlers/handleIncomingMessages.js
+++ webSocket-without-auth/backend/handlers/handleIncomingMessages.js
@@ -1,14 +1,25 @@
 import { saveMsgFromWebsocketToDb } from "../controllers/messages.js";
+import { saveDislike, saveLike } from "../controllers/reaction.js";
 
 export const handleIncomingMessages = async (message) => {
   const msgString = message.toString();
 
   try {
     const messageData = JSON.parse(msgString);
-    const newMessage = await saveMsgFromWebsocketToDb(messageData);
-    return newMessage;
+
+    switch (messageData.type) {
+      case "like":
+        return { ...(await saveLike(messageData)), type: "like" };
+      case "dislike":
+        return { ...(await saveDislike(messageData)), type: "dislike" };
+      default:
+        return {
+          ...(await saveMsgFromWebsocketToDb(messageData)),
+          type: "message",
+        };
+    }
   } catch (error) {
     console.error("Error parsing message", error);
-    return null; // Return null on error
+    return null;
   }
 };
