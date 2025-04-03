ad72db46ddeccddca2bdd2e7121be748a3dbd4f7
eediallo <elhhabdouldiallo@gmail.com>
2025-04-03 11:08:22 +0100

    feat: add endpoint to retrieve likes and dislikes count and fix dislike function import

--- webSocket-without-auth/backend/routes/reaction.js
+++ webSocket-without-auth/backend/routes/reaction.js
@@ -1,9 +1,14 @@
 import express from "express";
-import { likeMessage, DislikeMessage } from "../controllers/reaction.js";
+import {
+  likeMessage,
+  dislikeMessage,
+  getLikesAndDislikes,
+} from "../controllers/reaction.js";
 
 const reactionRouter = express.Router();
 
 reactionRouter.post("/:messageId/like", likeMessage);
-reactionRouter.post("/:messageId/dislike", DislikeMessage);
+reactionRouter.post("/:messageId/dislike", dislikeMessage);
+reactionRouter.get("/:messageId/reactions", getLikesAndDislikes);
 
 export { reactionRouter };
