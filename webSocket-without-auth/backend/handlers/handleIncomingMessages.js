c9911745f98da604e51ea51a9b4af0610addbef5
eediallo <elhhabdouldiallo@gmail.com>
2025-04-04 14:30:15 +0100

    feat: enhance message handling to include user information and improve response structure

--- webSocket-without-auth/backend/handlers/handleIncomingMessages.js
+++ webSocket-without-auth/backend/handlers/handleIncomingMessages.js
@@ -1,19 +1,24 @@
 import { saveMsgFromWebsocketToDb } from "../controllers/messages.js";
 import { saveDislike, saveLike } from "../controllers/reaction.js";
 
-export const handleIncomingMessages = async (message) => {
-  const msgString = message.toString();
-
+export const handleIncomingMessages = async (message, ws, userConnection) => {
+  const dataString = message.toString();
+  console.log(dataString, "INCOMING MESSAGE");
   try {
-    const messageData = JSON.parse(msgString);
-    
-    switch (messageData.type) {
+    const data = JSON.parse(dataString);
+    const userInfo = userConnection.get(ws);
+    console.log(userInfo, "user info");
+    switch (data.type) {
       case "like":
-        return await saveLike(messageData);
+        const { _doc: like } = await saveLike(data.userInfo.userId);
+        return { ...like, type: "like" };
       case "dislike":
-        return await saveDislike(messageData);
+        const { _doc: dislike } = await saveDislike(data, userInfo.userId);
+        return { ...dislike, type: "dislike" };
+
       default:
-        return await saveMsgFromWebsocketToDb(messageData);
+        const { _doc: message } = await saveMsgFromWebsocketToDb(data);
+        return { ...message, type: "message" };
     }
   } catch (error) {
     console.error("Error parsing message", error);
