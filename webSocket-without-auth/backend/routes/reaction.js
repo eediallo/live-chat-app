5577ba8b6dcc6ffef841e05cb6dcc78b2f31308e
eediallo <elhhabdouldiallo@gmail.com>
2025-04-04 15:00:06 +0100

    feat: remove like and dislike routes to simplify reaction handling

--- webSocket-without-auth/backend/routes/reaction.js
+++ webSocket-without-auth/backend/routes/reaction.js
@@ -1,14 +1,8 @@
 import express from "express";
-import {
-  getLikesAndDislikes,
-  likeMessage,
-  dislikeMessage,
-} from "../controllers/reaction.js";
+import { getLikesAndDislikes } from "../controllers/reaction.js";
 
 const reactionRouter = express.Router();
 
-reactionRouter.post("/:messageId/:userId/like", likeMessage);
-reactionRouter.post("/:messageId/:userId/dislike", dislikeMessage);
 reactionRouter.get("/:messageId", getLikesAndDislikes);
 
 export { reactionRouter };
