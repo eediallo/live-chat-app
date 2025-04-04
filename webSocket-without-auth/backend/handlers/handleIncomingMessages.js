1b77848147d5927aa50fb07c2cc50674921ca193
eediallo <elhhabdouldiallo@gmail.com>
2025-04-04 18:09:49 +0100

    feat: update like and dislike handling to return accurate counts

--- webSocket-without-auth/backend/handlers/handleIncomingMessages.js
+++ webSocket-without-auth/backend/handlers/handleIncomingMessages.js
@@ -12,16 +12,16 @@ export const handleIncomingMessages = async (message, ws, userConnection) => {
     const { messageId } = data;
     const userInfo = userConnection.get(ws); // get user info from ws
     const userId = userInfo?.userId;
-    const likeDislikeCounts = await getLikesDislikeCounts(messageId);
-    console.log(userInfo, "user info");
     switch (data.type) {
       case "like":
-        console.log(likeDislikeCounts, "counts===>>>>>>");
         const { _doc: like } = await saveLike(data, userId);
-        return { ...like, type: "like", ...likeDislikeCounts };
+        const likesCounts = await getLikesDislikeCounts(messageId);
+        console.log(likesCounts, "likesCounts");
+        return { ...like, type: "like", ...likesCounts };
       case "dislike":
         const { _doc: dislike } = await saveDislike(data, userId);
-        return { ...dislike, type: "dislike", ...likeDislikeCounts };
+        const dislikesCounts = await getLikesDislikeCounts(messageId);
+        return { ...dislike, type: "dislike", ...dislikesCounts };
 
       default:
         const { _doc: message } = await saveMsgFromWebsocketToDb(data);
