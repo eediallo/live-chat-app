c40e419854869866f7f8c4d5c8a13ed46b6ff65f
eediallo <elhhabdouldiallo@gmail.com>
2025-04-04 16:35:41 +0100

    feat: implement reaction counts retrieval and enhance message handling in WebSocket

--- webSocket-without-auth/backend/controllers/reaction.js
+++ webSocket-without-auth/backend/controllers/reaction.js
@@ -27,6 +27,19 @@ export const saveLike = async (likeData, userId) => {
   }
 };
 
+export const getLikesDislikeCounts = async (messageId) => {
+  try {
+    const [likes, dislikes] = await Promise.all([
+      Like.countDocuments({ messageId }),
+      Dislike.countDocuments({ messageId }),
+    ]);
+    return { likes, dislikes };
+  } catch (err) {
+    console.error("Failed to get likes and dislikes counts");
+    return null;
+  }
+};
+
 export const saveDislike = async (dislikeData, userId) => {
   const { messageId } = dislikeData;
   try {
