68420a565d69b116fe782cbf6e03aaa88d453212
eediallo <elhhabdouldiallo@gmail.com>
2025-04-04 23:34:46 +0100

    ref: move database operation for saving messages into handlers folder

--- webSocket-without-auth/backend/controllers/messages.js
+++ webSocket-without-auth/backend/controllers/messages.js
@@ -1,36 +1,5 @@
 import { StatusCodes } from "http-status-codes";
 import { Message } from "../models/message.js";
-import { User } from "../models/user.js";
-
-export const saveMsgFromWebsocketToDb = async (message) => {
-  const { sender, text, createdAt } = message;
-  const username = sender.username;
-
-  if (!username || !text) {
-    console.error("Username and text message must be provided");
-    return;
-  }
-  try {
-    let existingUser = await User.findOne({ username: username });
-    if (!existingUser) {
-      existingUser = await User.create({ username: username });
-    }
-
-    const newMessage = await Message.create({
-      sender: {
-        id: existingUser._id,
-        username: username,
-      },
-      text,
-      createdAt,
-    });
-
-    return newMessage;
-  } catch (e) {
-    console.log(e);
-    return null;
-  }
-};
 
 // Get all messages for all users
 export const getAllMessagesForAllUsers = async (req, res) => {
