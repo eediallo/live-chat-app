1aaef9e8d2b2e025b0141e50de193d96ba364a08
eediallo <elhhabdouldiallo@gmail.com>
2025-04-06 16:29:40 +0100

    feat: implement pagination for fetching messages and return total count

--- webSocket-without-auth/backend/controllers/messages.js
+++ webSocket-without-auth/backend/controllers/messages.js
@@ -3,25 +3,30 @@ import { Message } from "../models/message.js";
 
 // Get all messages for all users
 export const getAllMessagesForAllUsers = async (req, res) => {
-  const { since } = req.query;
-  let query = {};
+  const { since, page, limit = 5 } = req.query;
 
-  if (since) {
-    const sinceDate = new Date(since);
-    if (!isNaN(sinceDate)) {
-      query.createdAt = { $gte: sinceDate };
-    }
-  }
+  const query = since ? { createdAt: { $gte: new Date(since) } } : {};
 
   try {
-    const messages = await Message.find(query).sort("createdAt");
+    const totalMessages = await Message.countDocuments(query);
+    const numOfPages = Math.ceil(totalMessages / Number(limit));
+
+    // If no page is provided, default to the last page
+    const currentPage = page ? Number(page) : numOfPages;
+    const skip = (currentPage - 1) * Number(limit);
+
+    const messages = await Message.find(query)
+      .sort("createdAt")
+      .skip(skip)
+      .limit(Number(limit));
+
     if (messages.length === 0) {
       return res
         .status(StatusCodes.NOT_FOUND)
         .json({ msg: "No messages found" });
     }
 
-    res.status(StatusCodes.OK).json({ messages });
+    res.status(StatusCodes.OK).json({ messages, totalMessages, numOfPages });
   } catch (error) {
     console.error(error);
     res
