da9f897015277f6dfce8790f9e49d1657e6028b4
eediallo <elhhabdouldiallo@gmail.com>
2025-04-06 17:05:31 +0100

    refactor: simplify message fetching by removing unused query parameters

--- webSocket-without-auth/backend/controllers/messages.js
+++ webSocket-without-auth/backend/controllers/messages.js
@@ -1,30 +1,21 @@
 import { StatusCodes } from "http-status-codes";
 import { Message } from "../models/message.js";
 
-// Get all messages for all users
 export const getAllMessagesForAllUsers = async (req, res) => {
-  const { since, page, limit = 5 } = req.query;
-
-  const query = since ? { createdAt: { $gte: new Date(since) } } : {};
+  const { page, limit = 5 } = req.query;
 
   try {
-    const totalMessages = await Message.countDocuments(query);
+    const totalMessages = await Message.countDocuments();
     const numOfPages = Math.ceil(totalMessages / Number(limit));
 
     // If no page is provided, default to the last page
     const currentPage = page ? Number(page) : numOfPages;
     const skip = (currentPage - 1) * Number(limit);
 
-    const messages = await Message.find(query)
+    const messages = await Message.find()
       .sort("createdAt")
       .skip(skip)
       .limit(Number(limit));
-
-    if (messages.length === 0) {
-      return res
-        .status(StatusCodes.NOT_FOUND)
-        .json({ msg: "No messages found" });
-    }
 
     res.status(StatusCodes.OK).json({ messages, totalMessages, numOfPages });
   } catch (error) {
