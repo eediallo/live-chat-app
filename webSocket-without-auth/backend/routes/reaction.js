144dfca66d1a966d3ba3a92eac33128625cba925
eediallo <elhhabdouldiallo@gmail.com>
2025-04-07 12:04:37 +0100

    feat: add getMessageLikes and getAllReactions routes to reactionRouter

--- webSocket-without-auth/backend/routes/reaction.js
+++ webSocket-without-auth/backend/routes/reaction.js
@@ -1,8 +1,14 @@
 import express from "express";
-import { getMessageReactionCounts } from "../controllers/reaction.js";
+import {
+  getMessageReactionCounts,
+  getMessageLikes,
+  getAllReactions,
+} from "../controllers/reaction.js";
 
 const reactionRouter = express.Router();
 
+reactionRouter.get("/", getAllReactions);
 reactionRouter.get("/:messageId", getMessageReactionCounts);
+reactionRouter.get("/:messageId", getMessageLikes);
 
 export { reactionRouter };
