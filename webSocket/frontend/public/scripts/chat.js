1b643f0d07c164cd67a9c9dff7ab41e92f1664b6
eediallo <elhhabdouldiallo@gmail.com>
2025-04-13 21:02:22 +0100

    feat: enhance editMessagePayload to include sender info and timestamp

--- webSocket/frontend/public/scripts/chat.js
+++ webSocket/frontend/public/scripts/chat.js
@@ -141,6 +141,7 @@ export async function dislikeMessagePayload(messageId) {
   };
   socket.send(JSON.stringify(payload));
 }
+
 export async function deleteMessagePayload(messageId) {
   const payload = {
     type: "delete",
@@ -149,10 +150,28 @@ export async function deleteMessagePayload(messageId) {
   socket.send(JSON.stringify(payload));
 }
 
-export async function editMessagePayload(messageId) {
-  const payload = {
-    type: "edit",
-    messageId: messageId,
-  };
-  socket.send(JSON.stringify(payload));
+export async function editMessagePayload(messageId, newText) {
+  try {
+    if (socket.readyState === WebSocket.OPEN) {
+      const timestamp = new Date().toISOString();
+      const payload = {
+        type: "edit",
+        newText,
+        messageId,
+        sender: {
+          name: userInfo.name,
+        },
+        updatedAt: timestamp,
+      };
+      socket.send(JSON.stringify(payload));
+      // Hide the error message if it is visible
+      errorMsgEl.style.display = "none";
+    } else {
+      console.error("WebSocket is not open. Cannot send message.");
+    }
+  } catch (e) {
+    if (e.code === 11000) {
+      console.log("11000 error");
+    }
+  }
 }
