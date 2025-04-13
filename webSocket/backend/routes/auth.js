83a9c1dd49501c5500dfe96f1a05834d0cd0e4c9
eediallo <elhhabdouldiallo@gmail.com>
2025-04-13 11:49:11 +0100

    feat: add route to retrieve the number of users

--- webSocket/backend/routes/auth.js
+++ webSocket/backend/routes/auth.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { login, register } from "../controllers/auth.js";
+import { getNumberOfUsers, login, register } from "../controllers/auth.js";
 
 const authRouter = express.Router();
 
 authRouter.route("/login").post(login);
 authRouter.route("/register").post(register);
+authRouter.route("/users_number").get(getNumberOfUsers);
 
 export { authRouter };
