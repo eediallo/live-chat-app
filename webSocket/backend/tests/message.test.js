16b4fbfe9d9fefc8876b0f792a305f59bc4643b6
eediallo <elhhabdouldiallo@gmail.com>
2025-05-04 12:29:33 +0100

    test: add unit tests for createMessage function in message model

---
+++ webSocket/backend/tests/message.test.js
@@ -0,0 +1,46 @@
+import { vi, it, expect, beforeEach, afterEach, describe } from "vitest";
+import request from "supertest";
+import app from "../app";
+
+import { Message } from "../models/message";
+import { StatusCodes } from "http-status-codes";
+
+vi.mock("../middleware/auth", () => ({
+  authenticateUser: vi.fn((req, res, next) => {
+    req.user = { id: "938383", name: "Mick" }; // Mock authenticated user
+    next();
+  }),
+}));
+
+describe("createMessage()", () => {
+  let createMock;
+
+  beforeEach(() => {
+    createMock = vi.spyOn(Message, "create");
+  });
+
+  afterEach(() => {
+    createMock.mockRestore();
+  });
+
+  it(`should create a message and return ${StatusCodes.CREATED} status`, async () => {
+    const user = { userID: "938383", name: "Mick" };
+    const messageData = { message: "Hello" };
+    const createdMessage = {
+      sender: { id: user.userID, name: user.name },
+      message: messageData.message,
+    };
+
+    createMock.mockResolvedValue(createdMessage);
+
+    const response = await request(app)
+      .post("/api/v1/messages")
+      .set("Authorization", "Bearer valid-token")
+      .send(messageData);
+
+    expect(response.status).toBe(StatusCodes.CREATED);
+    expect(response.body).toHaveProperty("msg", "Message created successfully");
+    expect(response.body).toHaveProperty("message");
+    expect(response.body.message).toMatchObject(createdMessage);
+  });
+});
