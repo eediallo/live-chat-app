f97bbb380bba52dccbbd6f0ad49827ceb238b541
eediallo <elhhabdouldiallo@gmail.com>
2025-05-05 13:12:51 +0100

    refactor: reorganize WebSocket initialization and token import in state.js

--- webSocket/frontend/scripts/state.js
+++ webSocket/frontend/scripts/state.js
@@ -1,3 +1,5 @@
+import { token } from "./data.js";
+
 export const state = {
   messages: [],
   name: null,
@@ -10,3 +12,7 @@ export const state = {
   isSocket: false,
   baseUrl: "https://eediallo-chat-server-auth.hosting.codeyourfuture.io/api/v1",
 };
+
+export let socket = new WebSocket(
+  `wss://eediallo-chat-server-auth.hosting.codeyourfuture.io/?token=${token}`
+);
