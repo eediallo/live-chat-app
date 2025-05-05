8b6c49ac6c47f624942a913fb0c51a4a32a52a10
eediallo <elhhabdouldiallo@gmail.com>
2025-05-05 13:12:17 +0100

    refactor: streamline WebSocket initialization and move message payload functions to a new module

--- webSocket/frontend/scripts/chat.js
+++ webSocket/frontend/scripts/chat.js
@@ -21,12 +21,9 @@ import {
   updateMessageReactionsUI,
   updatePaginationControls,
 } from "./render.js";
+import { socket } from "./state.js";
 
 redirectIfNotAuthenticated();
-
-let socket = new WebSocket(
-  `wss://eediallo-chat-server-auth.hosting.codeyourfuture.io/?token=${token}`
-);
 
 socket.onopen = async () => {
   usernameEl.textContent = `${userInfo.name}`; // display name of connected user
@@ -153,53 +150,3 @@ function sendMessageHandler(e) {
 }
 
 sendMsgBtn.addEventListener("click", sendMessageHandler);
-
-export async function likeMessagePayload(messageId) {
-  const payload = {
-    type: "like",
-    messageId: messageId,
-  };
-  socket.send(JSON.stringify(payload));
-}
-
-export async function dislikeMessagePayload(messageId) {
-  const payload = {
-    type: "dislike",
-    messageId: messageId,
-  };
-  socket.send(JSON.stringify(payload));
-}
-
-export async function deleteMessagePayload(messageId) {
-  const payload = {
-    type: "delete",
-    messageId: messageId,
-  };
-  socket.send(JSON.stringify(payload));
-}
-
-export async function editMessagePayload(messageId, newText) {
-  try {
-    if (socket.readyState === WebSocket.OPEN) {
-      const timestamp = new Date().toISOString();
-      const payload = {
-        type: "edit",
-        newText,
-        messageId,
-        sender: {
-          name: userInfo.name,
-        },
-        updatedAt: timestamp,
-      };
-      socket.send(JSON.stringify(payload));
-      // Hide the error message if it is visible
-      errorMsgEl.style.display = "none";
-    } else {
-      console.error("WebSocket is not open. Cannot send message.");
-    }
-  } catch (e) {
-    if (e.code === 11000) {
-      console.log("11000 error");
-    }
-  }
-}
