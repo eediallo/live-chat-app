ba49f20f0a6883bb48a783be8816d3d3a7e1fc7a
eediallo <elhhabdouldiallo@gmail.com>
2025-05-06 13:26:36 +0100

    test: add event listener test for login button functionality

---
+++ webSocket/frontend/tests/events.test.js
@@ -0,0 +1,28 @@
+import fs from "fs";
+import path from "path";
+
+import { vi, it, describe, expect } from "vitest";
+import { Window } from "happy-dom";
+
+// ensure the content of the of login page is available
+const htmlDocPath = path.join(process.cwd(), "login.html");
+const htmlDocumentContent = fs.readFileSync(htmlDocPath).toString();
+
+const window = new Window();
+const document = window.document;
+document.write(htmlDocumentContent);
+vi.stubGlobal("document", document);
+
+const loginBtn = document.getElementById("login-btn");
+
+describe("loginBtn", () => {
+  it("should execute loginUser function when loginBtn is clicked", () => {
+    const loginUser = vi.fn();
+
+    expect(loginBtn).not.toBeNull();
+    loginBtn.addEventListener("click", loginUser);
+    loginBtn.click();
+
+    expect(loginUser).toHaveBeenCalled();
+  });
+});
