fdbc16070d6d3c98ef06d3072729cc5c3d32a5c7
eediallo <elhhabdouldiallo@gmail.com>
2025-05-06 19:24:05 +0100

    test: add tests for dislikeMessagePayload function

--- webSocket/frontend/tests/payloads.test.js
+++ webSocket/frontend/tests/payloads.test.js
@@ -1,5 +1,5 @@
 import { vi, it, describe, expect } from "vitest";
-import { likeMessagePayload } from "../scripts/payloads";
+import { likeMessagePayload, dislikeMessagePayload } from "../scripts/payloads";
 
 const socket = {
   send: vi.fn(),
@@ -9,12 +9,25 @@ vi.mock("../scripts/payloads", () => ({
   likeMessagePayload: (messageId) => {
     socket.send(JSON.stringify({ type: "like", messageId }));
   },
+  dislikeMessagePayload: (messageId) => {
+    socket.send(JSON.stringify({ type: "dislike", messageId }));
+  },
 }));
 
 describe("likeMessagePayload()", () => {
   it("should send liked message with id 123", () => {
     const messageId = 123;
     likeMessagePayload(messageId);
+    expect(socket.send).toHaveBeenCalledWith(
+      JSON.stringify({ type: "like", messageId })
+    );
+  });
+});
+
+describe("dislikeMessagePayload()", () => {
+  it("should send disliked message with id 123", () => {
+    const messageId = 123;
+    dislikeMessagePayload(messageId);
     expect(socket.send).toHaveBeenCalledWith(
       JSON.stringify({ type: "like", messageId })
     );
